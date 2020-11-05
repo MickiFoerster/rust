3248bea7323e9efaeab923100d00be9874986e18
micki <github@com-science.de>
2020-11-05 21:09:02 +0100

    Add Mutex

--- reference-counting-atomic/src/main.rs
+++ reference-counting-atomic/src/main.rs
@@ -1,27 +1,33 @@
 use std::thread;
-use std::sync::Arc;
+use std::sync::{Arc,Mutex};
 
 struct Person {
-    name: Arc<String>
+    name: Arc<String>,
+    state: Arc<Mutex<String>> // If you need to change the component then use Mutex
 }
 
 impl Person {
-    fn new(name: Arc<String>) -> Person {
-        Person { name: name }
+    fn new(name: Arc<String>, state: Arc<Mutex<String>>) -> Person {
+        Person { name: name, state: state }
     }
 
     fn greet(&self) {
-        println!("Hi, my name is {}", self.name);
+
+        let mut state = self.state.lock().unwrap();
+        state.clear();
+        state.push_str("excited");
+        println!("Hi, my name is {} and I'm {}", self.name, state.as_str());
     }
 }
 
 fn rc_demo() {
     let name = Arc::new("John".to_string());
-    let person = Person::new(name.clone()); // clone() increases the reference count
+    let state = Arc::new(Mutex::new("bored".to_string()));
+    let person = Person::new(name.clone(), state.clone()); // clone() increases the reference count
     let t = thread::spawn(move || {
         person.greet(); // thread print greeting
     });
-    println!("Name = {}", name); // main thread should print name
+    println!("Name = {}, state = {}", name, state.lock().unwrap().as_str()); // main thread should print name
 
     t.join().unwrap(); // join thread
 }
