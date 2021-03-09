a60794c9554d653954bea3dea0e7669ac5887f86
micki <github@com-science.de>
2021-03-09 21:53:10 +0100

    Change html fetcher

--- html-fetcher/src/main.rs
+++ html-fetcher/src/main.rs
@@ -1,3 +1,19 @@
+extern crate hyper;
+
+use hyper::Client;
+
 fn main() {
-    println!("Hello, world!");
+    println!("HELLO");
 }
+//#[tokio::main]
+//async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//    let client = Client::new();
+//
+//    let uri = "http://www.com-science.de/".parse()?;
+//
+//    let rep = client.get(uri).await?;
+//
+//    println!("Response: {}", resp.status());
+//
+//    Ok(())
+//}
