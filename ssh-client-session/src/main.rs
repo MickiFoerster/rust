955d8ac1bbb78c97a6371fd6c2635fe41579b084
micki <github@com-science.de>
2022-11-06 13:20:32 +0100

    ssh session example

--- ssh-client-session/src/main.rs
+++ ssh-client-session/src/main.rs
@@ -1,17 +1,50 @@
 use ssh2::Session;
+use std::io::prelude::*;
+use tokio::net::TcpStream;
 
-fn main() {
-    // Almost all APIs require a `Session` to be available
-    let sess = Session::new().unwrap();
-    let mut agent = sess.agent().unwrap();
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
 
-    // Connect the agent and request a list of identities
-    agent.connect().unwrap();
-    agent.list_identities().unwrap();
+    if args.len() < 2 {
+        eprintln!("Give remote host name or IP address as parameters e.g. rpi1 rpi2 rpi3 ...");
 
-    for identity in agent.identities().unwrap() {
-        println!("{}", identity.comment());
-        let _pubkey = identity.blob();
+        std::process::exit(1);
+    }
+
+    let mut sessions = vec![];
+
+    for rpi in args[1..].iter() {
+        println!("Argument {rpi}");
+        let sess = ssh_session("pi", format!("{rpi}:22"));
+        sessions.push(sess);
+    }
+
+    for fut in sessions {
+        fut.await.unwrap();
     }
 }
 
+async fn ssh_session(user: &str, host: String) -> Result<(), String> {
+    println!("Connecting to {host} as user {user}");
+    let tcp = match TcpStream::connect(host).await {
+        Ok(v) => v,
+        Err(err) => {
+            println!("connect error: {err}");
+            return Err("Could not connect to remote host".into());
+        }
+    };
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_agent(user).unwrap();
+
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+    channel.wait_close().unwrap();
+    println!("{}", channel.exit_status().unwrap());
+    Ok(())
+}
