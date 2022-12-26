d1159e68292681a50ca9577d41c593fad9f43b25
micki <github@com-science.de>
2022-12-26 20:14:44 +0100

    Update exif tool

--- exif-created-date-mv/src/lib.rs
+++ exif-created-date-mv/src/lib.rs
@@ -1,9 +1,11 @@
 use std::str::FromStr;
+use std::path::Path;
 
+use walkdir::WalkDir;
 use chrono::{DateTime, Utc};
 use exif::{In, Tag};
 
-pub fn get_creation_time(path: &str) -> Option<DateTime<Utc>> {
+pub fn get_creation_time(path: &Path) -> Option<DateTime<Utc>> {
         let file = std::fs::File::open(path).ok()?;
         let mut bufreader = std::io::BufReader::new(&file);
         let exifreader = exif::Reader::new();
@@ -11,4 +13,19 @@ pub fn get_creation_time(path: &str) -> Option<DateTime<Utc>> {
         let f = exif.get_field(Tag::DateTimeOriginal, In::PRIMARY)?;
         let date_str = format!("{}Z", f.display_value());
         DateTime::from_str(&date_str).ok()
+}
+
+pub fn recursive_search(path: &Path) {
+    for entry in WalkDir::new(path)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| { 
+            //println!("{:?}", e); 
+            e.ok()
+        })
+    {
+        if let Some(create_date) = get_creation_time(entry.path()) {
+            println!("{}    {}", entry.path().display(), create_date);
+        }
+    }
 }
--- exif-created-date-mv/src/main.rs
+++ exif-created-date-mv/src/main.rs
@@ -1,13 +1,8 @@
-use exif_created_date_mv::get_creation_time;
+use exif_created_date_mv::*;
+use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    for path in &["/home/micki/Pictures/IMG_20180208_073937.jpg"] {
-        match get_creation_time(path) {
-            Some(d) => println!("{}: {}", path, d),
-            None => println!("No creation date found"),
-        }
-    }
+    recursive_search(Path::new("/home/micki/Pictures"));
 
     Ok(())
 }
