6bf4d497f4e4b2a69d7d45f90aab9bbca735e13b
micki <github@com-science.de>
2022-12-27 09:16:59 +0100

    create dest dir from date

--- exif-created-date-mv/src/lib.rs
+++ exif-created-date-mv/src/lib.rs
@@ -1,15 +1,15 @@
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Datelike, Timelike, Utc};
 use exif::{In, Tag};
 use walkdir::WalkDir;
 
 #[derive(Debug, Clone)]
-struct MediaFile {
-    name: String,
-    path: PathBuf,
-    create_date: DateTime<Utc>,
+pub struct MediaFile {
+    pub name: String,
+    pub path: PathBuf,
+    pub create_date: DateTime<Utc>,
 }
 
 pub fn get_creation_time(path: &Path) -> Option<DateTime<Utc>> {
@@ -22,7 +22,7 @@ pub fn get_creation_time(path: &Path) -> Option<DateTime<Utc>> {
     DateTime::from_str(&date_str).ok()
 }
 
-pub fn recursive_search(path: &Path) {
+pub fn recursive_search(path: &Path) -> Vec<MediaFile> {
     let mut files = Vec::new();
     for entry in WalkDir::new(path)
         .follow_links(true)
@@ -44,7 +44,25 @@ pub fn recursive_search(path: &Path) {
         }
     }
 
+    files
+}
+
+pub fn move_files_to_dest_dir(
+    files: Vec<MediaFile>,
+    dest_dir: &Path,
+) -> Result<(), std::io::Error> {
     for f in files.iter() {
-        println!("{:#?}", f);
+        let path = get_dest_dir_path(dest_dir, &f.create_date);
+        println!("path: {}", path.display());
+        std::fs::create_dir_all(path).expect("directory cannot be created");
     }
+
+    Ok(())
+}
+
+fn get_dest_dir_path(base_dir: &Path, date: &DateTime<Utc>) -> PathBuf {
+    base_dir
+        .join(format!("{:04}", date.year()))
+        .join(format!("{:02}", date.month()))
+        .join(format!("{:02}", date.day()))
 }
--- exif-created-date-mv/src/main.rs
+++ exif-created-date-mv/src/main.rs
@@ -2,7 +2,12 @@ use exif_created_date_mv::*;
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    recursive_search(Path::new("/home/micki/Pictures"));
+    let files = recursive_search(Path::new("/home/micki/Pictures"));
+    for f in files.iter() {
+        println!("{} : {}", f.path.display(), f.create_date);
+    }
+
+    move_files_to_dest_dir(files, Path::new("/tmp/asdf"))?;
 
     Ok(())
 }
