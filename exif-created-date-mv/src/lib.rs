251e6d476caff3d6e95abdf16b603a58dd828e08
micki <github@com-science.de>
2022-12-30 23:55:22 +0100

    hash

--- exif-created-date-mv/src/lib.rs
+++ exif-created-date-mv/src/lib.rs
@@ -1,8 +1,10 @@
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
 use chrono::{DateTime, Datelike, Timelike, Utc};
 use exif::{In, Tag};
+use sha2::{Digest, Sha256};
 use walkdir::WalkDir;
 
 #[derive(Debug, Clone)]
@@ -11,16 +13,29 @@ pub struct MediaFile {
     pub path: PathBuf,
     pub len: u64,
     pub create_date: Option<DateTime<Utc>>,
+    pub hash: Option<String>,
+}
+
+fn get_hash_of_file(path: &Path) -> Option<String> {
+    let mut file = std::fs::File::open(path).ok()?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).ok()?;
+    let mut hasher = Sha256::new();
+    hasher.update(buffer);
+    Some(format!("{:X}", hasher.finalize()))
 }
 
 fn get_media_file(path: &Path) -> Option<MediaFile> {
     let file = std::fs::File::open(path).ok()?;
+
     let mut media_file = MediaFile {
         name: path.file_name()?.to_string_lossy().into(),
         path: PathBuf::from(path),
         len: file.metadata().ok()?.len(),
+        hash: get_hash_of_file(path),
         create_date: None,
     };
+
     let mut bufreader = std::io::BufReader::new(&file);
     let exifreader = exif::Reader::new();
     let exif = exifreader.read_from_container(&mut bufreader).ok()?;
@@ -80,7 +95,7 @@ pub fn copy_files_to_dest_dir(source_dir: &Path, dest_dir: &Path) -> Result<usiz
         match f.create_date {
             Some(d) => {
                 path = get_dest_dir_path(dest_dir, &d);
-                let new_filename = get_dest_filename(&f.name, d);
+                let new_filename = get_dest_filename(&f, d);
                 dest_file_path = path.join(new_filename);
             }
             None => {
@@ -130,15 +145,21 @@ fn copy_media_file(
     Ok(())
 }
 
-fn get_dest_filename(old_filename: &str, d: DateTime<Utc>) -> PathBuf {
+fn get_dest_filename(file: &MediaFile, d: DateTime<Utc>) -> PathBuf {
+    let filename_ext = file
+        .name
+        .split('.')
+        .last()
+        .unwrap_or(&file.name[file.name.len() - 3..]);
     let new_filename = format!(
-        "{:04}-{:02}-{:02}_{:02}_{:02}-{:02}",
+        "{:04}-{:02}-{:02}_{:02}_{:02}-{}.{}",
         d.year(),
         d.month(),
         d.day(),
         d.hour(),
         d.minute(),
-        old_filename
+        file.hash.clone().unwrap_or_default(),
+        filename_ext.to_lowercase()
     );
 
     PathBuf::from(new_filename)
