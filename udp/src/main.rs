ce293b9970a357d02e97f0d585efba2d3bed3af1
micki <github@com-science.de>
2023-01-03 19:22:15 +0100

    UDP example

---
+++ udp/src/main.rs
@@ -0,0 +1,21 @@
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    {
+        let socket = UdpSocket::bind("127.0.0.1:34254")?;
+
+        loop {
+            // Receives a single datagram message on the socket. If `buf` is too small to hold
+            // the message, it will be cut off.
+            let mut buf = [0; 10];
+            let (amt, src) = socket.recv_from(&mut buf)?;
+
+            // Redeclare `buf` as slice of the received data and send reverse data back to origin.
+            let buf = &mut buf[..amt];
+            print!("{}: {:?}\n", amt, buf);
+            buf.reverse();
+            socket.send_to(buf, &src)?;
+        }
+    } // the socket is closed here
+    Ok(())
+}
