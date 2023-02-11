4f180853cab2131560d4d0bfa736942646b16bce
micki <github@com-science.de>
2023-02-11 14:44:35 +0100

    cargo fmt

--- serde_yaml_example/src/main.rs
+++ serde_yaml_example/src/main.rs
@@ -1,5 +1,5 @@
+use serde::Deserialize;
 use std::collections::{BTreeMap, HashMap};
-use serde::{Deserialize};
 
 #[derive(Debug, Hash, PartialEq, Eq, Deserialize)]
 enum Language {
@@ -9,14 +9,14 @@ enum Language {
 
 fn main() -> Result<(), serde_yaml::Error> {
     let yaml = std::fs::read_to_string("input.yaml").expect("could not read file");
-    let deserialized_map: BTreeMap<String, HashMap<Language, String>> = serde_yaml::from_str(&yaml)?;
+    let deserialized_map: BTreeMap<String, HashMap<Language, String>> =
+        serde_yaml::from_str(&yaml)?;
     println!("{:#?}", deserialized_map);
 
     println!("{}", deserialized_map["label1"][&Language::English]);
     println!("{}", deserialized_map["label2"][&Language::English]);
     println!("{}", deserialized_map["label3"][&Language::English]);
     println!("{}", deserialized_map["label4"][&Language::English]);
-
 
     println!("{}", deserialized_map["label1"][&Language::Deutsch]);
     println!("{}", deserialized_map["label2"][&Language::Deutsch]);
