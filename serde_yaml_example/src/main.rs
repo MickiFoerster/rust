9977e05d9b3cf17a12ef59a23ece7493b6c06082
micki <github@com-science.de>
2023-02-11 09:00:10 +0100

    Add example for translation data from YAML file

---
+++ serde_yaml_example/src/main.rs
@@ -0,0 +1,27 @@
+use std::collections::{BTreeMap, HashMap};
+use serde::{Deserialize};
+
+#[derive(Debug, Hash, PartialEq, Eq, Deserialize)]
+enum Language {
+    Deutsch,
+    English,
+}
+
+fn main() -> Result<(), serde_yaml::Error> {
+    let yaml = std::fs::read_to_string("input.yaml").expect("could not read file");
+    let deserialized_map: BTreeMap<String, HashMap<Language, String>> = serde_yaml::from_str(&yaml)?;
+    println!("{:#?}", deserialized_map);
+
+    println!("{}", deserialized_map["label1"][&Language::English]);
+    println!("{}", deserialized_map["label2"][&Language::English]);
+    println!("{}", deserialized_map["label3"][&Language::English]);
+    println!("{}", deserialized_map["label4"][&Language::English]);
+
+
+    println!("{}", deserialized_map["label1"][&Language::Deutsch]);
+    println!("{}", deserialized_map["label2"][&Language::Deutsch]);
+    println!("{}", deserialized_map["label3"][&Language::Deutsch]);
+    println!("{}", deserialized_map["label4"][&Language::Deutsch]);
+
+    Ok(())
+}
