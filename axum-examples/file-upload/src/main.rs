5f0f422f2a1aa69ad980bec9dc941b0742339d7e
micki <github@com-science.de>
2023-04-15 20:41:58 +0200

    axum: file upload example

--- axum-examples/file-upload/src/main.rs
+++ axum-examples/file-upload/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::{DefaultBodyLimit, Multipart},
     response::{Html, IntoResponse},
     routing::get,
     Router,
@@ -9,7 +10,7 @@ use std::net::SocketAddr;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(show_form).post(accept_form));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
     axum::Server::bind(&addr)
@@ -18,20 +19,43 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> impl IntoResponse {
+async fn show_form() -> impl IntoResponse {
     Html(format!(
         r#"
         <html>
         <body>
+        
+<form action="/" method="post" enctype="multipart/form-data">
 <label for="avatar">Choose a profile picture:</label>
 
 <input type="file"
        id="avatar" name="avatar"
        accept="image/png, image/jpeg">
 
+<input type="submit" value="Upload">
+        </form>
+
         </body>
         </html>
             "#
     ))
     .into_response()
+}
+
+async fn accept_form(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let file_name = field.file_name().unwrap().to_string();
+        let content_type = field.content_type().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        println!(
+            "Length of `{}` (`{}`: `{}`) is {} bytes",
+            name,
+            file_name,
+            content_type,
+            data.len()
+        );
+        std::fs::write("/tmp/upload", data).expect("write to file failed");
+    }
 }
