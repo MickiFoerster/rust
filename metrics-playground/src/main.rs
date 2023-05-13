789ab88399fac7da0cce257581d0e8a9c8752cb0
micki <github@com-science.de>
2023-05-13 22:21:35 +0200

    Example prometheus grafana

--- metrics-playground/src/main.rs
+++ metrics-playground/src/main.rs
@@ -1,76 +1,121 @@
-fn main() {
-    // By default `prometheus::default_registry()` is used.
-    let recorder = metrics_prometheus::install();
+use axum::{
+    extract::MatchedPath,
+    http::Request,
+    middleware::{self, Next},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
+use std::{
+    future::ready,
+    net::SocketAddr,
+    time::{Duration, Instant},
+};
+use tower_http::trace::DefaultMakeSpan;
+use tower_http::trace::DefaultOnResponse;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-    // Either use `metrics` crate interfaces.
-    metrics::increment_counter!("count", "whose" => "mine", "kind" => "owned");
-    metrics::increment_counter!("count", "whose" => "mine", "kind" => "ref");
-    metrics::increment_counter!("count", "kind" => "owned", "whose" => "dummy");
+fn main_app() -> Router {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    // Or construct and provide `prometheus` metrics directly.
-    recorder.register_metric(prometheus::Gauge::new("value", "help")?);
+    Router::new()
+        .route("/hello", get(|| async { "Hello World" }))
+        .route(
+            "/slow",
+            get(|| async {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }),
+        )
+        .route_layer(middleware::from_fn(track_metrics))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        )
+}
 
-    let report = prometheus::TextEncoder::new()
-        .encode_to_string(&prometheus::default_registry().gather())?;
-    assert_eq!(
-        report.trim(),
-        r#"
-# HELP count count
-# TYPE count counter
-count{kind="owned",whose="dummy"} 1
-count{kind="owned",whose="mine"} 1
-count{kind="ref",whose="mine"} 1
-# HELP value help
-# TYPE value gauge
-value 0
-    "#
-        .trim(),
-    );
+async fn start_main_server() -> Result<(), hyper::Error> {
+    let app = main_app();
 
-    // Metrics can be described anytime after being registered in
-    // `prometheus::Registry`.
-    metrics::describe_counter!("count", "Example of counter.");
-    metrics::describe_gauge!("value", "Example of gauge.");
+    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+}
 
-    let report = prometheus::TextEncoder::new().encode_to_string(&recorder.registry().gather())?;
-    assert_eq!(
-        report.trim(),
-        r#"
-# HELP count Example of counter.
-# TYPE count counter
-count{kind="owned",whose="dummy"} 1
-count{kind="owned",whose="mine"} 1
-count{kind="ref",whose="mine"} 1
-# HELP value Example of gauge.
-# TYPE value gauge
-value 0
-    "#
-        .trim(),
-    );
+async fn start_metrics_server() -> Result<(), hyper::Error> {
+    let app = metrics_app();
 
-    // Description can be changed multiple times and anytime.
-    metrics::describe_counter!("count", "Another description.");
+    // NOTE: expose metrics endpoint on a different port
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+}
 
-    // Even before a metric is registered in `prometheus::Registry`.
-    metrics::describe_counter!("another", "Yet another counter.");
-    metrics::increment_counter!("another");
+fn setup_metrics_recorder() -> PrometheusHandle {
+    const EXPONENTIAL_SECONDS: &[f64] = &[
+        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+    ];
 
-    let report = prometheus::TextEncoder::new().encode_to_string(&recorder.registry().gather())?;
-    assert_eq!(
-        report.trim(),
-        r#"
-# HELP another Yet another counter.
-# TYPE another counter
-another 1
-# HELP count Another description.
-# TYPE count counter
-count{kind="owned",whose="dummy"} 1
-count{kind="owned",whose="mine"} 1
-count{kind="ref",whose="mine"} 1
-# HELP value Example of gauge.
-# TYPE value gauge
-value 0
-    "#
-        .trim(),
-    );
+    PrometheusBuilder::new()
+        .set_buckets_for_metric(
+            Matcher::Full("http_requests_duration_seconds".to_string()),
+            EXPONENTIAL_SECONDS,
+        )
+        .unwrap()
+        .install_recorder()
+        .unwrap()
+}
+
+fn metrics_app() -> Router {
+    let recorder_handle = setup_metrics_recorder();
+    Router::new().route("/metrics", get(move || ready(recorder_handle.render())))
+}
+
+async fn track_metrics<B>(req: Request<B>, next: Next<B>) -> impl IntoResponse {
+    let start = Instant::now();
+    let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
+        matched_path.as_str().to_owned()
+    } else {
+        req.uri().path().to_owned()
+    };
+    let method = req.method().clone();
+
+    let response = next.run(req).await;
+
+    let latency = start.elapsed().as_secs_f64();
+    let status = response.status().as_u16().to_string();
+
+    let labels = [
+        ("method", method.to_string()),
+        ("path", path),
+        ("status", status),
+    ];
+
+    metrics::increment_counter!("http_requests_total", &labels);
+    metrics::histogram!("http_requests_duration_seconds", latency, &labels);
+
+    response
+}
+
+#[tokio::main]
+async fn main() {
+    let main_server = async {
+        start_main_server().await.unwrap();
+    };
+    let metric_server = async {
+        start_metrics_server().await.unwrap();
+    };
+    tokio::select! {
+        reason = main_server => { println!("main server exits: {:?}", reason); }
+        reason = metric_server => { println!("main server exits: {:?}", reason); }
+    }
 }
