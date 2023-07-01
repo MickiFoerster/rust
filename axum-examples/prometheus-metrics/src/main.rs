63556e5ba9636e2f7e29423c0cbc7a376868fbfb
micki <github@com-science.de>
2023-07-01 09:23:33 +0200

    only two simple counters

--- axum-examples/prometheus-metrics/src/main.rs
+++ axum-examples/prometheus-metrics/src/main.rs
@@ -16,26 +16,49 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 fn metrics_app() -> Router {
     let recorder_handle = setup_metrics_recorder();
-    Router::new().route("/metrics", get(move || ready(recorder_handle.render())))
+    Router::new().route(
+        "/metrics",
+        get(move || {
+            tracing::info!(
+                "metrics endpoint was called: rendering metrics with prometheus recorder",
+            );
+            ready(recorder_handle.render())
+        }),
+    )
 }
 
 fn main_app() -> Router {
+    tokio::spawn(async move {
+        loop {
+            tracing::trace!("concurrent thread for incrementing counter ...");
+            tokio::time::sleep(Duration::from_millis(500)).await;
+            metrics::increment_counter!("counter_all_fivehundret_ms_total", &[("key", "value")]);
+        }
+    });
+
     Router::new()
-        .route("/fast", get(|| async {}))
+        .route(
+            "/fast",
+            get(|| async {
+                tracing::info!("increase counter my_testing_counter_total");
+                metrics::increment_counter!("my_testing_counter_total", &[("route", "fast")]);
+            }),
+        )
         .route(
             "/slow",
             get(|| async {
                 tokio::time::sleep(Duration::from_secs(1)).await;
+                metrics::increment_counter!("my_testing_counter_total", &[("route", "slow")]);
             }),
         )
-        .route_layer(middleware::from_fn(track_metrics))
+    //.route_layer(middleware::from_fn(track_metrics))
 }
 
 async fn start_main_server() {
     let app = main_app();
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    tracing::debug!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -46,7 +69,7 @@ async fn start_metrics_server() {
     let app = metrics_app();
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
-    tracing::debug!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -70,21 +93,21 @@ async fn main() {
 }
 
 fn setup_metrics_recorder() -> PrometheusHandle {
-    const EXPONENTIAL_SECONDS: &[f64] = &[
-        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
-    ];
+    // const EXPONENTIAL_SECONDS: &[f64] = &[
+    //     0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+    // ];
 
     PrometheusBuilder::new()
-        .set_buckets_for_metric(
-            Matcher::Full("http_requests_duration_seconds".to_string()),
-            EXPONENTIAL_SECONDS,
-        )
-        .unwrap()
+        // .set_buckets_for_metric(
+        //     Matcher::Full("http_requests_duration_seconds".to_string()),
+        //     EXPONENTIAL_SECONDS,
+        // )
+        // .unwrap()
         .install_recorder()
         .unwrap()
 }
 
-async fn track_metrics<B>(req: Request<B>, next: Next<B>) -> impl IntoResponse {
+async fn _track_metrics<B>(req: Request<B>, next: Next<B>) -> impl IntoResponse {
     let start = Instant::now();
     let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
         matched_path.as_str().to_owned()
