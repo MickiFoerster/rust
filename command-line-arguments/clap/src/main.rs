e8c074a4791927564b45cd8e086e80efc011ca3c
micki <github@com-science.de>
2023-09-22 07:07:23 +0200

    bring example to recent clap version

--- command-line-arguments/clap/src/main.rs
+++ command-line-arguments/clap/src/main.rs
@@ -1,39 +1,37 @@
-use clap::{App, Arg};
+use clap::{value_parser, Arg};
 
 fn main() {
-    let matches = App::new("My Program")
+    let matches = clap::Command::new("My Program")
         .arg(
-            Arg::with_name("verbose")
-                .short("v")
+            Arg::new("verbose")
+                .short('v')
                 .long("verbose")
+                .action(clap::ArgAction::SetTrue)
                 .help("Enable verbosity"),
         )
         .arg(
-            Arg::with_name("jobs")
-                .short("j")
+            Arg::new("jobs")
+                .short('j')
                 .long("jobs")
                 .value_name("NUM")
-                .help("Number of jobs")
-                .takes_value(true),
+                .value_parser(value_parser!(usize))
+                .default_value("1")
+                .help("Number of jobs"),
         )
         .arg(
-            Arg::with_name("name")
-                .short("n")
+            Arg::new("name")
+                .short('n')
                 .long("name")
                 .value_name("NAME")
                 .help("Name argument")
-                .takes_value(true)
                 .default_value("myname"),
         )
         .get_matches();
 
-    let verbose = matches.is_present("verbose");
-    let jobs = matches.value_of("jobs").map(|v| v.parse::<u32>().unwrap());
-    let name = matches.value_of("name").unwrap();
-
-    println!("Verbose: {}", verbose);
-    if let Some(jobs) = jobs {
-        println!("Jobs: {}", jobs);
-    }
-    println!("Name: {}", name);
+    let verbose = matches.get_flag("verbose");
+    let jobs: usize = *matches.get_one("jobs").expect("'job' is invalid");
+    let name = matches
+        .get_one::<String>("name")
+        .expect("'name' is invalid");
+    dbg!(verbose, jobs, name);
 }
